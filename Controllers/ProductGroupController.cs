1737fafe4a5ec904ad5216ff03f4872bb173bc99
Carl Jesus Lomaad <carljesuslomaad@gmail.com>
2025-04-07 10:53:01 +0800

    Add project files.

---
+++ Controllers/ProductGroupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FairySkin.Controllers
+{
+    public class ProductGroupController : Controller
+    {
+        // GET: ProductGroup
+        public ActionResult ProductGroup()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FairySkin.MvcApplication" Language="C#" %>
