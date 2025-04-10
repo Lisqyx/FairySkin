3f8baa1598f709fb1cbd6ee02bdcb47b035071d2
Carl Jesus Lomaad <carljesuslomaad@gmail.com>
2025-04-10 10:59:38 +0800

    Updated

---
+++ Controllers/InventoryOutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FairySkin.Controllers
+{
+    public class InventoryOutController : Controller
+    {
+        // GET: InventoryOut
+        public ActionResult InventoryOut()
+        {
+            return View();
+        }
+    }
+}
