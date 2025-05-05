f77de2776f2f1eee4d2ac9ea26c9d02f129cc527
Carl Jesus Lomaad <carljesuslomaad@gmail.com>
2025-05-05 11:39:40 +0800

    Updated

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FairySkin.Controllers
+{
+    public class InventoryController : Controller
+    {
+        // GET: Inventory
+        public ActionResult InventoryIn()
+        {
+            return View();
+        }
+        public ActionResult InventoryInNew()
+        {
+            return View();
+        }
+        public ActionResult InventoryOut()
+        {
+            return View();
+        }
+        public ActionResult InventoryOutNew()
+        {
+            return View();
+        }
+        public ActionResult InventoryInReport()
+        {
+            return View();
+        }
+        public ActionResult InventoryInReportNew()
+        {
+            return View();
+        }
+        public ActionResult InventoryOutReport()
+        {
+            return View();
+        }
+        public ActionResult InventoryOutReportNew()
+        {
+            return View();
+        }
+        public ActionResult InventoryReportPerLocation()
+        {
+            return View();
+        }
+        public ActionResult InventoryReportSummary()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/StocksController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FairySkin.Controllers
+{
+    public class StocksController : Controller
+    {
+        // GET: Stocks
+        public ActionResult StockAdjustment()
+        {
+            return View();
+        }
+        public ActionResult StockAdjustmentNew()
+        {
+            return View();
+        }
+        public ActionResult StockReconciliation()
+        {
+            return View();
+        }
+        public ActionResult StockReplacement()
+        {
+            return View();
+        }
+        public ActionResult StockReplacementNew()
+        {
+            return View();
+        }
+        public ActionResult StockReturn()
+        {
+            return View();
+        }
+        public ActionResult StockReturnNew()
+        {
+            return View();
+        }
+        public ActionResult DefectiveStocksReport()
+        {
+            return View();
+        }
+    }
+}
